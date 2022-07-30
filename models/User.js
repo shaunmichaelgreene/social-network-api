4738171a79372c4bc1bf26c48fa00729c1bd9490
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-30 00:05:24 -0400

    models added

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+constUserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /d/ //insert regex
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+            }
+        ],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    // return this.friends.reduce(
+    //     (total, friend) => total + friend.length + 1, 0
+    // );
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
